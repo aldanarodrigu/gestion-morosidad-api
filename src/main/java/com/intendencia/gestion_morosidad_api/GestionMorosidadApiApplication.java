ef0e1ce11e3799d7bef28bec357ffb0fe7eb8061
aldanarodrigu <aldanarodriguez5407@gmail.com>
2026-09-16 21:18:55 -0300

    Inicializar proyecto backend

---
+++ src/main/java/com/intendencia/gestion_morosidad_api/GestionMorosidadApiApplication.java
@@ -0,0 +1,13 @@
+package com.intendencia.gestion_morosidad_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMorosidadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMorosidadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intendencia/gestion_morosidad_api/GestionMorosidadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intendencia.gestion_morosidad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMorosidadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
